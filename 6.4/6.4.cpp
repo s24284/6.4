41b70eb8c4e44bba8344dc4e965667ba1a8d258d
Anna Prymas <s24284@pjwstk.edu.pl>
2021-12-18 13:27:07 +0100

    Dodaj pliki projektów.

---
+++ 6.4/6.4.cpp
@@ -0,0 +1,39 @@
+﻿// 6.4.cpp :
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string tekst;
+	int ilosc;
+	std::string sumawszystkichtekstow;
+
+	
+	std::cout << "Podaj mi jakis ciekawy tekst, nie uzywajac spacji:\n";
+	std::cin >> tekst;
+
+	do {
+		std::cout << "Podaj, ile razy mam dokonać konkatenacji twojego tekstu:\n";
+		std::cin >> ilosc;
+
+		if (ilosc <= 0)
+		{
+			std::cout << "By dokonac konkatenacji, potrzebuje liczby wiekszej od 0. Podaj ja jeszcze raz:\n";
+		}
+	} 	while (ilosc <= 0);
+
+
+	for (int i = 0; i <= ilosc; i++)
+	{
+		sumawszystkichtekstow += tekst;
+
+	}
+
+	std::cout << sumawszystkichtekstow;
+
+		return 0;
+
+}
+
